71dcd3411dd28b1d9763741d0323fc43c96f3325
Andre Baltieri <andrebaltieri@hotmail.com>
2021-05-07 15:25:33 -0300

    Iniciando o projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace comecando_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
